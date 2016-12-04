f48d40470ded200af93962b89fb18929db868508
yagi <yagi@Miyalab-Mac-Pro.local>
2016-12-04 21:11:09 +0900

    add log

--- Yagi/TTM/ttm/run.sh
+++ Yagi/TTM/ttm/run.sh
@@ -1,3 +1,3 @@
-nohup python ttm.py -f baskets/baskets_per_month -d 11194 -v 5 -k 5 -p 12 -i 100 &
-nohup python ttm.py -f baskets/baskets_per_month -d 11194 -v 5 -k 7 -p 12 -i 100 &
-nohup python ttm.py -f baskets/baskets_per_month -d 11194 -v 5 -k 10 -p 12 -i 100 &
+nohup python ttm.py -f ../data/baskets_kmeans_10/baskets_per_month -d 11194 -v 10 -k 5 -p 12 -i 100 &
+nohup python ttm.py -f ../data/baskets_kmeans_10/baskets_per_month -d 11194 -v 10 -k 7 -p 12 -i 100 &
+nohup python ttm.py -f ../data/baskets_kmeans_10/baskets_per_month -d 11194 -v 10 -k 10 -p 12 -i 100 &
